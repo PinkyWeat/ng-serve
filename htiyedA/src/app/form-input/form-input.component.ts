2d1aa2ea02b48c7fb0cd55fab1b974e8c8a19536
Denisse Landau <denisselandau97@gmail.com>
2024-11-15 20:07:12 -0300

    nicer looking forms, still way to go

--- htiyedA/src/app/form-input/form-input.component.ts
+++ htiyedA/src/app/form-input/form-input.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-form-input',
-  templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.scss']
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss'],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule]
 })
-export class FormInputComponent {
-  @Input() form!: FormGroup;
-  @Input() controlName!: string;
-  @Input() label!: string;
-  @Input() type: string = 'text';
-  @Input() placeholder: string = '';
+export class UserFormComponent implements OnInit {
+  userForm!: FormGroup;
+  formSubmitted = false;
 
-  get isInvalid(): boolean {
-    const control = this.form.get(this.controlName);
-    return control ? control.invalid && control.touched : false;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      birthdate: ['', Validators.required],
+      country: ['', Validators.required],
+      referral: [''] // Optional field, no Validators.required
+    });
   }
+
+  onSubmit() {
+    console.log('Form Submitted:', this.userForm.valid);
+    console.log('Form Data:', this.userForm.value);
+    this.formSubmitted = true;
+  
+    if (this.userForm.valid) {
+      alert('Mock email sent successfully!');
+      this.userForm.reset();
+      this.formSubmitted = false;
+    } else {
+      console.log('Form is invalid. Errors:', this.userForm.errors);
+    }
+  }
+  
 }
--- htiyedA/src/app/user-form/user-form.component.ts
+++ htiyedA/src/app/user-form/user-form.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss'],
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule]
+  imports: [FormsModule, ReactiveFormsModule, CommonModule]
 })
 export class UserFormComponent implements OnInit {
   userForm!: FormGroup;
+  formSubmitted = false;
 
   constructor(private fb: FormBuilder) {}
 
@@ -25,6 +28,7 @@ export class UserFormComponent implements OnInit {
   }
 
   onSubmit() {
+    this.formSubmitted = true;
     if (this.userForm.valid) {
       console.log('Form Data:', this.userForm.value);
       alert('Mock email sent successfully!');
